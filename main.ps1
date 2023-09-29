6cc13ddbbfff63864bcd3dba3bc1acb7e1c0740a
Darin Bolton <dbolton@uttyler.edu>
2023-09-29 17:30:39 -0500

    Getting started

---
+++ main.ps1
@@ -0,0 +1,8 @@
+<#
+.SYNOPSIS
+    Queries SC2Pulse for StarCraft II ladder statistics
+.NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.LINK
+    https://github.com/sc2-pulse && https://github.com/darinbolton/LadderTracker
+#>
