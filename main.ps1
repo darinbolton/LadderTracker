ae6e444f8c9516b46123ae3c60c3f0e57382637b
Darin Bolton <dbolton@uttyler.edu>
2023-10-02 23:07:58 -0500

    Sorta works, not yet

--- main.ps1
+++ main.ps1
@@ -8,10 +8,19 @@
     https://sc2pulse.nephest.com/sc2/doc/swagger-ui/index.html?configUrl=/sc2/v3/api-docs/swagger-config#/character-controller/getCharacterSummary_1
 #>
 
-# Values
+# Import NephestID, Race, and Name
 $playerIDs = Import-Csv "C:\Users\Darin\Code\LadderTracker\NephestIDs.csv"
+$date = Get-Date -Format "MM-dd-yyyy"
 
 $playerIDs | ForEach-Object {
-    $mmr = Invoke-WebRequest -Uri "https://sc2pulse.nephest.com/sc2/api/character/$($_.NephestID)/summary/1v1/7/$($_.Race)" | ConvertFrom-Json
-    Write-Host "$($_.Name)"$mmr.ratingLast
+    $mmr = Invoke-WebRequest -Uri "https://sc2pulse.nephest.com/sc2/api/character/$($_.NephestID)/summary/1v1/7/$($_.Race)"`
+    | ConvertFrom-Json `
+    | Select-Object -Property $($_.Name),ratingLast `
+    | Add-Content -Path "C:\Users\Darin\Code\LadderTracker\MMR\$date.txt"
 }
+
+
+
+
+
+#Write-Host "$($_.Name)"$mmr.ratingLast
