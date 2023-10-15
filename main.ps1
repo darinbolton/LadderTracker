eaa2a6f4d3937f7519817ea528bc7e3efc31da35
Darin Bolton <dbolton@uttyler.edu>
2023-10-15 00:39:37 -0500

    Pulls name and MMR correctly now

--- main.ps1
+++ main.ps1
@@ -8,19 +8,26 @@
     https://sc2pulse.nephest.com/sc2/doc/swagger-ui/index.html?configUrl=/sc2/v3/api-docs/swagger-config#/character-controller/getCharacterSummary_1
 #>
 
-# Import NephestID, Race, and Name
-$playerIDs = Import-Csv "C:\Users\Darin\Code\LadderTracker\NephestIDs.csv"
+$playerIDs = Import-Csv .\NephestIDs.csv
 $date = Get-Date -Format "MM-dd-yyyy"
 
-$playerIDs | ForEach-Object {
-    $mmr = Invoke-WebRequest -Uri "https://sc2pulse.nephest.com/sc2/api/character/$($_.NephestID)/summary/1v1/7/$($_.Race)"`
-    | ConvertFrom-Json `
-    | Select-Object -Property $($_.Name),ratingLast `
-    | Add-Content -Path "C:\Users\Darin\Code\LadderTracker\MMR\$date.txt"
+$apiResponses = @()
+
+foreach ($row in $playerIDs){
+
+    $name = $row.Name
+    $NephestID = $row.NephestID
+    $race = $row.Race
+    $response = Invoke-WebRequest -Uri "https://sc2pulse.nephest.com/sc2/api/character/$NephestID/summary/1v1/7/$race"
+    $response1 = $response.Content | ConvertFrom-Json
+    $mmr = $response1.ratingLast
+
+    $nameRequest = Invoke-WebRequest -Uri "https://sc2pulse.nephest.com/sc2/api/character/$NephestID"
+    $name = $nameRequest.Content | ConvertFrom-Json
+
+    # Add the API response to the array
+    $apiResponses += $name.Name + ";" + $response1.ratingLast
+    
 }
 
-
-
-
-
-#Write-Host "$($_.Name)"$mmr.ratingLast
+$apiResponses| ConvertFrom-String -Delimiter ";" -PropertyNames Name, MMR | Select-Object -Property Name, MMR | Export-Csv -Path .\MMR\$date.csv
